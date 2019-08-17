ce1dd451bc621d20b0b9203cc2749c274a3fc168
Jack Hazlehurst <jack@jackhazlehurst.com>
2019-08-17 07:11:08 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_0hBmTOkrC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6un1d2hv0t5ava9u32ec0tk27k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://master.dctqee8c7wgvo.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };

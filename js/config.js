52400e31a5f08deadab686c94e2f09c5c47e7f4e
Jack Hazlehurst <jack@jackhazlehurst.com>
2019-08-20 02:00:25 +0000

    configure api invokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://master.dctqee8c7wgvo.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://53pk21cg79.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
